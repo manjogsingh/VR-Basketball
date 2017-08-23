44b464c368b5bb14fc85e604fa830a205def2803
manjogsingh <manjogsingh@gmail.com>
2017-08-24 02:47:59 +0530

    New Triggers Added

--- Assets/Scripts/BallBehaviour.cs
+++ Assets/Scripts/BallBehaviour.cs
@@ -1,42 +1,56 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class BallBehaviour : MonoBehaviour {
+public class BallBehaviour : MonoBehaviour
+{
 
-	private Rigidbody rb;
+    private Rigidbody rb;
 
-	public Transform ballPortal;
-	public GameObject endCanvas;
-	void Start()
-	{
-		rb=GetComponent<Rigidbody>();
-	}
-	public void BallReset()
-	{
-		transform.position=ballPortal.position;
-		rb.angularVelocity=Vector3.zero;
-		rb.velocity=Vector3.zero;
-	}
+    public Transform ballPortal;
+    public GameObject endCanvas;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    public void BallReset()
+    {
+        transform.position = ballPortal.position;
+        rb.angularVelocity = Vector3.zero;
+        rb.velocity = Vector3.zero;
+    }
 
-	void OnCollisionEnter(Collision other)
-	{
-		if(other.gameObject.CompareTag("Null Space"))
-		{
-			BallReset();
-		}
-	}
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Null Space"))
+        {
+            BallReset();
+        }
+    }
 
-	void OnTriggerEnter(Collider other)
-	{
-		if(other.gameObject.name.Equals("Restart"))
-		{
-			endCanvas.SetActive(false);
-			Timer.timeLeft=90f;
-		}
-		else if(other.gameObject.name.Equals("Exit"))
-		{
-			//exit
-		}
-	}
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name.Equals("Restart"))
+        {
+            endCanvas.SetActive(false);
+            Timer.timeLeft = 90f;
+        }
+        else if (other.gameObject.name.Equals("Exit"))
+        {
+            SteamVR_LoadLevel.Begin("Intro");
+        }
+        else if (other.gameObject.name.Equals("Practice-Half"))
+        {
+            SteamVR_LoadLevel.Begin("Practice-Half");
+        }
+        else if (other.gameObject.name.Equals("Practice-Full"))
+        {
+            SteamVR_LoadLevel.Begin("Practice-Full");
+        }
+        else if (other.gameObject.name.Equals("RapidFire"))
+        {
+            SteamVR_LoadLevel.Begin("RapidFire");
+        }
+    }
 }
